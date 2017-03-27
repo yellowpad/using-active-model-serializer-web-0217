a9df723689487e62b987ba09ab2a9edf533cb06d
dgwp-projects <zayneabraham1@gmail.com>
2017-03-27 15:31:46 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,9 @@
 class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @author }
+    end   
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}]) }
+      format.json { render json: @post }
     end
   end
 
@@ -32,12 +31,12 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def post_data
-    post = Post.find(params[:id])
-    #render json: PostSerializer.serialize(post)
-    render json: post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}])
-  end
+  # def post_data
+  #   post = Post.find(params[:id])
+  #   #render json: PostSerializer.serialize(post)
+  #   render json: post.to_json(only: [:title, :description, :id],
+  #                             include: [author: { only: [:name]}])
+  # end
 
 private
   # Use callbacks to share common setup or constraints between actions.
